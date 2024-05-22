b788dd7c290c9a32eb9915ad66df02d860be0b1c
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-05-22 14:42:29 +0000

    answer buttons colour change and next button only when answered

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ const answers = document.getElementById("answers")
 let shuffq, currqindex
 
 startbtn.addEventListener('click', gamestart)
+nextbtn.addEventListener('click', nextq)
 
 function gamestart() {
     console.log("Started!")
@@ -20,6 +21,13 @@ function gamestart() {
 
 function nextq() {
     showq(shuffq[currqindex])
+    currqindex++; // Increment the current question index
+    if (currqindex < shuffq.length) {
+        showq(); // Show the next question
+    } else {
+        // Handle end of quiz (if desired)
+        console.log("End of quiz reached!");
+    }
 }
 const questions = [
     {
@@ -30,13 +38,43 @@ const questions = [
             {text: 'Caterpillars', correct: false},
             {text: 'Wasps', correct: false}
         ]
+    },
+    {
+        question: 'Methuselah, an ancient Bristlecone Pine and one of the oldest living trees in the world is estimated to be around how many years old?',
+        answers: [
+            {text: '3854', correct: false},
+            {text: '4253', correct: false},
+            {text: '4528', correct: false},
+            {text: '4852', correct: true}
+        ]
     }
 ]
 function showq(question) {
+     nextbtn.classList.add('hide');
     const currentQuestion = shuffq[currqindex];
     qelement.innerText = currentQuestion.question;
-    
+    answers.innerHTML = ''; // Clear previous answers
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn','col-sm-5', 'offset-sm-1');
+        button.addEventListener('click', () => answerQuestion(answer.correct));
+        answers.appendChild(button); // Append button to answers container
+    });
 }
 
-function answer() {}
-
+function answerQuestion(isCorrect) {
+    if (isCorrect) {
+        // Handle correct answer
+        console.log('Correct!');
+        const selectedButton = event.target;
+        selectedButton.classList.add('correct');
+    } else {
+        // Handle incorrect answer
+        console.log('Incorrect!');
+        const selectedButton = event.target;
+        selectedButton.classList.add('wrong');
+    }
+    nextbtn.classList.remove('hide'); // Show the Next button
+}
